e3f6a2c638c52a5fe975e14091c9dc13fb080a67
LaYurinella <theory.toe@gmail.com>
2023-04-26 15:45:01 +0700

    edit prompt

--- chunk-display.js
+++ chunk-display.js
@@ -52,7 +52,7 @@ function copyToClipboard(text) {
   
 	  // Display the current chunk
 	  const chunkElem = document.createElement("p");
-	  chunkElem.textContent = chunks[chunkIndex] + "; Rephrase the above text into a more concise, clearer and in everyday language but preserve the details. If the text seems to be missing the start, use the unfinished part of the earlier prompt to complete the sentence.";
+	  chunkElem.textContent = chunks[chunkIndex] + "; Rephrase the above text into a more concise, clearer and in everyday language but preserve the details.";
 	  wordCount.appendChild(chunkElem);
   
 	  const copyButton = document.createElement("button");
